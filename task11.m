93816cc940e7e0a41e936622e0190811efe30b1b
saramirandaa <0244643@up.edu.mx>
2022-06-27 21:55:25 -0500

    Reporte/tasks

--- task11.m
+++ task11.m
@@ -3,5 +3,34 @@ clc;
 clearvars;
 close all;
 %% 
+original = imread("sarayleal.jpeg");
+original = (rgb2gray(original));
 
-img = imread("sarayleal.jpeg");
+imGauss = imnoise(original, "gaussian");
+imSP = imnoise(original, "salt & pepper");
+imPoi = imnoise(original, "poisson");
+imSpk = imnoise(original, "speckle");
+%uno(original);
+figure("Name","Sobel"); 
+uno(imGauss, "Gaussiano", 1);
+uno(imSP, "Salt&Pepper", 2);
+uno(imPoi, "Poisson", 3);
+uno(imSpk, "Speckle", 4);
+figure("Name", "Filtros");
+function uno = uno(img, titulo, num);
+    sobel1 = edge(img, 'sobel');
+    %imshow(sobel1);
+    subplot(2,2,num); imshow(sobel1); title(titulo);
+    dos(img, titulo, num);
+end
+
+function dos = dos(img2, titulo, num);
+    k = ones(3); k = k/3;
+    prom = imfilter(img2,k);
+    med = medfilt2(img2, [3,3]);
+    gauss = imgaussfilt(img2, 2, 'FilterSize', 5);
+    subplot(2,2,num); imshow(prom); title(titulo);
+    subplot(2,2,num); imshow(med); title(titulo);
+    subplot(2,2,num); imshow(gauss); title(titulo);
+end
+
--- task8.m
+++ task8.m
@@ -2,55 +2,55 @@
 %% Fitlro Promedio 
 clc; clearvars; close all;
 baja = imread("coffee.jpg");
-alta = imread("amigos.png");
+alta = imread("hr.png");
 baja = rgb2gray(baja); alta = rgb2gray(alta);
 figure(); imshow(alta); title("Original1");
-figure(); imshow(baja); title("Original2");
+%figure(); imshow(baja); title("Original2");
 
-k1 = ones(3); k2 = ones(7); k3 = ones(11);
-k1 = k1/9; k2 = k2/49; k3 = k3/121;
+k1 = ones(52); k2 = ones(7); k3 = ones(11);
+k1 = k1/2704; k2 = k2/49; k3 = k3/121;
 
 imF = imfilter(baja,k1); imF2 = imfilter(alta,k1);
-figure(); imshow(imF, []); title("Kernel 3x3");
-figure(); imshow(imF2, []); title("Kernel 3x3");
-imF = imfilter(baja,k2); imF2 = imfilter(alta,k2);
-figure(); imshow(imF, []); title("Kernel 7x7");
-figure(); imshow(imF2, []); title("Kernel 7x7");
-imF = imfilter(baja,k3); imF2 = imfilter(alta,k3);
-figure(); imshow(imF, []); title("Kernel 11x11");
+figure(); imshow(imF, []); title("Kernel 52x52");
+%figure(); imshow(imF2, []); title("Kernel 3x3");
+%imF = imfilter(baja,k2); imF2 = imfilter(alta,k2);
+%figure(); imshow(imF, []); title("Kernel 7x7");
+%figure(); imshow(imF2, []); title("Kernel 7x7");
+%imF = imfilter(baja,k3); imF2 = imfilter(alta,k3);
+%figure(); imshow(imF, []); title("Kernel 11x11");
 figure(); imshow(imF2, []); title("Kernel 11x11");
 
 %% Filtro Mediana
 clc; clearvars; close all;
 baja = imread("coffee.jpg");
-alta = imread("amigos.png");
+alta = imread("hr.png");
 baja = rgb2gray(baja); alta = rgb2gray(alta);
-figure(); imshow(alta); title("Original1");
-figure(); imshow(baja); title("Original2");
+%figure(); imshow(alta); title("Original1");
+%figure(); imshow(baja); title("Original2");
 
-imFM1 = medfilt2(baja, [5,5]);
-imFM2 = medfilt2(alta, [5,5]);
-imFM3 = medfilt2(baja, [9,9]);
-imFM4 = medfilt2(alta, [9,9]);
-imFM5 = medfilt2(baja, [13,13]);
-imFM6 = medfilt2(alta, [52,52]);
+imFM1 = medfilt2(baja, [52,52]);
+imFM2 = medfilt2(alta, [52,52]);
+imFM3 = medfilt2(baja, [15,15]);
+imFM4 = medfilt2(alta, [15,15]);
+imFM5 = medfilt2(baja, [3,3]);
+imFM6 = medfilt2(alta, [3,3]);
 
-figure(); imshow(imFM1, []); title("BAJA 5x5");
-figure(); imshow(imFM3, []); title("BAJA 9x9");
-figure(); imshow(imFM5, []); title("BAJA 52x52");
+figure(); imshow(imFM1, []); title("BAJA 52x52");
+figure(); imshow(imFM3, []); title("BAJA 15x15");
+figure(); imshow(imFM5, []); title("BAJA 3x3");
 
-figure(); imshow(imFM2, []); title("ALTA 5x5");
-figure(); imshow(imFM4, []); title("ALTA 9x9");
-figure(); imshow(imFM6, []); title("ALTA 52x52");
+figure(); imshow(imFM2, []); title("ALTA 52x52");
+figure(); imshow(imFM4, []); title("ALTA 15x15");
+figure(); imshow(imFM6, []); title("ALTA 3x3");
 
 %% FILTRO GAUSSIANNO
 
 clc; clearvars; close all;
 baja = imread("coffee.jpg");
-alta = imread("amigos.png");
+alta = imread("hr.png");
 baja = rgb2gray(baja); alta = rgb2gray(alta);
 baja = double(baja); alta = double(alta);
-figure(); imshow(uint8(baja)); title("Original2");
+%figure(); imshow(uint8(baja)); title("Original2");
 
 imG = imgaussfilt(baja, 2, 'FilterSize', 5);
 imG2 = imgaussfilt(baja, 3, 'FilterSize', 7);
@@ -59,7 +59,7 @@ figure(); imshow(imG, []); title('2 y 5x5');
 figure(); imshow(imG2, []); title('3 y 7x7');
 figure(); imshow(imG3, []); title('8 y 9x9');
 
-figure(); imshow(uint8(alta)); title("Original1");
+%figure(); imshow(uint8(alta)); title("Original1");
 
 imG4 = imgaussfilt(alta, 10, 'FilterSize', 33);
 imG5 = imgaussfilt(alta, 15, 'FilterSize', 77);
